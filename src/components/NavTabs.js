0d57a6697f39f6a2e6c975b3cc0690b0eba32dbe
Seiya Miyazaki <smsmiyazaki@gmail.com>
2023-04-04 17:44:43 -0400

    Adjusting navTabs to adjust to the right

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
 function NavTabs({ currentPage, handlePageChange }) {
   return (
-    <ul className="nav nav-tabs">
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+      <h1 className="navbar-brand navTitle mx-auto">The Portfolio of Seiya Miyazaki</h1>
+  <button
+  className="navbar-toggler"
+  type="button"
+  data-bs-toggle="collapse"
+  data-bs-target="#navbarNav"
+  aria-controls="navbarNav"
+  aria-expanded="false"
+  aria-label="Toggle navigation"
+>
+  <span className="navbar-toggler-icon"></span>
+</button>
+<div className="collapse navbar-collapse" id="navbarNav">
+    <ul className="navbar-nav ms-auto">
       <li className="nav-item">
         <a
           href="#home"
@@ -47,6 +64,10 @@ function NavTabs({ currentPage, handlePageChange }) {
         </a>
       </li>
     </ul>
+    </div>
+    </div>
+  </nav>
+    </div>
   );
 }
 
