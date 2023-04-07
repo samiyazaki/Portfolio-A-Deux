54f15c2cb53b12a35d94964c20e7932e819d8e82
Seiya Miyazaki <smsmiyazaki@gmail.com>
2023-04-07 18:28:58 -0400

    Adding in favicon icon npms, adding styling for footer

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './NavTabs.css';
 
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
--- src/components/pages/Footer.js
+++ src/components/pages/Footer.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin, faStackOverflow } from '@fortawesome/free-brands-svg-icons';
 
 export default function Footer() {
     return (
         <footer className="footer">
             <a href ="https://github.com/samiyazaki" target="_blank" rel="noopener noreferrer">
-                <i className="fab fa-github"></i> github
+                <FontAwesomeIcon icon={faGithub} size="3x" />
                 </a>
             <a href="https://www.linkedin.com/in/seiya-miyazaki/" target="_blank" rel="noopener noreferrer">
-                <i className="fab fa-linkedin"></i> linkedin
+                <FontAwesomeIcon icon={faLinkedin} size="3x" />
                 </a>
                 <a href="https://stackoverflow.com/users/20827813/seiya-miyazaki" target="_blank" rel="noopener noreferrer">
-                <i className="fab fa-stack-overflow"></i> stack overflow
+                <FontAwesomeIcon icon={faStackOverflow} size="3x" />
                 </a>
         </footer>
     );
