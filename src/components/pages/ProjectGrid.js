2d4e8c1e5ecdc3f4092603b0e9d7c50f816158dd
Seiya Miyazaki <smsmiyazaki@gmail.com>
2023-04-07 13:55:04 -0400

    Adding more gifs and images to the projects page

--- src/components/pages/ProjectGrid.js
+++ src/components/pages/ProjectGrid.js
@@ -4,17 +4,19 @@ import './ProjectGrid.css';
 import Wallot from '../../assets/Wallot logo.png';
 import Walk from '../../assets/Take a Walk.gif';
 import Weather from '../../assets/Weather.gif';
+import Psycho from '../../assets/numbers-psychostick.gif';
+import Matilda from '../../assets/matilda-chocolate.gif'
 
 const projectsData = [
 
   { name: 'Wallot', link: 'https://wallot.herokuapp.com/', image: Wallot, },
   { name: 'Take A Walk', link: 'https://samiyazaki.github.io/Take_A_Walk/', image: Walk, },
   { name: 'Weather Dashboard', link: 'https://samiyazaki.github.io/Whatever_the_Weather/', image: Weather, },
-  { name: 'Counting App for tracking inventory', link: 'https://samiyazaki.github.io/Practice-counting-app/' },
-  { name: '5 to 1 carbohydrate calculator', link: 'https://samiyazaki.github.io/5_to_1/' },
+  { name: 'Counting App for tracking inventory', link: 'https://samiyazaki.github.io/Practice-counting-app/', image: Psycho, },
+  { name: '5 to 1 carbohydrate calculator', link: 'https://samiyazaki.github.io/5_to_1/', image: Matilda, },
   { name: 'Code Quiz', link: 'https://samiyazaki.github.io/Secular_Catechism/' },
 
-  
+  // On hover image switch from static to gif, event handler
    
 ];
 
