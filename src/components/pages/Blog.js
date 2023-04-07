c104be843e8f04f24ef10ca49e38584ce6a8a2b7
Seiya Miyazaki <smsmiyazaki@gmail.com>
2023-04-07 17:02:00 -0400

    Adjusting skills in CV

--- src/components/pages/Blog.js
+++ src/components/pages/Blog.js
@@ -7,7 +7,20 @@ export default function Blog() {
   const blogPosts = [
     {
       title: 'Skills',
-      content: ``,
+      content: ( <ul>
+        <li>Proficient in JavaScript (ES6+)</li>
+        <li>Experience with React</li>
+        <li>Experience with Node.js and Express</li>
+        <li>Experience with MongoDB and Mongoose</li>
+        <li>Strong understanding of RESTful APIs</li>
+        <li>Knowledge of Git and version control</li>
+        <li>Experience with HTML5 and CSS3</li>
+        <li>Understanding of responsive web design and CSS frameworks like Bootstrap and Tailwind CSS</li>
+        <li>Experience with Jest testing frameworks</li>
+        <li>Experience with build tools like Webpack, Babel, and npm</li>
+        <li>Understanding of web security and best practices</li>
+        <li>Strong problem-solving skills and ability to work independently or in a team</li>
+      </ul>),
     },
     {
       title: 'Click the link below to download my resume',
