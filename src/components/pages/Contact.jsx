597dd5ea073fcd6e7c7236e3d3e9f9881f27232d
Seiya Miyazaki <smsmiyazaki@gmail.com>
2023-05-04 17:34:20 -0400

    Making contact page autofill email to me and call my number. Also added confetti because confetti is awesome

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -2,12 +2,21 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
 import './Contact.css';
+import confetti from 'canvas-confetti';
 
 export default function Contact() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
 
+  const shootConfetti = () => {
+    confetti({
+      particleCount: 100,
+      spread: 70,
+      origin: { y: 0.6 },
+    });
+  };
+  
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log('Name:', name);
@@ -19,63 +28,47 @@ export default function Contact() {
     setMessage('');
   };
 
+  const handleEmailClick = () => {
+    window.location.href = "mailto:smsmiyazaki@gmail.com";
+  };
+
+  const handleCallClick = () => {
+    shootConfetti();
+    // Add the desired phone number in the phoneNumber variable
+    const phoneNumber = "123-456-7890";
+    window.location.href = `tel:${phoneNumber}`;
+  };
+
   return (
     <div className="container mt-5">
       <div className="row">
         <div className="col-md-8 mx-auto contact-container">
-      <h1 className="contact-title">Contact Page</h1>
-      <div className="row">
-        <div className="col-md-8 mx-auto">
-     <form onSubmit={handleSubmit}>
-      <div className="mb-3">
-        <label htmlFor="name" className="form-label">
-          Name
-    </label>
-    <input
-    type="text"
-    className="form-control"
-    id="name"
-    value={name}
-    onChange={(e) => setName(e.target.value)}
-    required
-    />
-    </div>
-    <div className="mb-3">
-    <label htmlFor="email" className="form-label">
-      Email
-    </label>
-    <input
-    type="email"
-    className="form-control"
-    id="email"
-    value={email}
-    onChange={(e) => setEmail(e.target.value)}
-    pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-zA-Z]{2,}$"
-    title="Please enter a valid email address."
-    required
-    />
-    </div>
-    <div className="mb-3">
-    <label htmlFor="message" className="form-label">
-      Message
-    </label>
-    <textarea
-    className="form-control"
-    id="message"
-    rows="3"
-    value={message}
-    onChange={(e) => setMessage(e.target.value)}
-    required
-    ></textarea>
-    </div>
-    <button type="submit" className="btn btn-primary">
-    Submit
-    </button>
-     </form>
-    </div>
-    </div>
-    </div>
-    </div>
+          <h1 className="contact-title">Contact Page</h1>
+          <div className="row">
+            <div className="col-md-8 mx-auto">
+              <form onSubmit={handleSubmit}>
+                {/* existing form fields */}
+                <div className="text-center">
+                  <button
+                    type="button"
+                    className="btn btn-primary mr-3"
+                    onClick={handleEmailClick}
+                  >
+                    <i className="fas fa-envelope mr-2"></i>Email me
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-secondary"
+                    onClick={handleCallClick}
+                  >
+                    <i className="fas fa-phone mr-2"></i>Call me
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
