a1fe33fdc6990f766233f1219d50009f1c44bbbe
Seiya Miyazaki <smsmiyazaki@gmail.com>
2023-05-30 17:33:29 -0400

    Punching up styling for navbar and contact page

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -26,15 +26,14 @@ export default function Contact() {
     setName('');
     setEmail('');
     setMessage('');
+    shootConfetti();
   };
 
   const handleEmailClick = () => {
-    shootConfetti();
     window.location.href = "mailto:smsmiyazaki@gmail.com";
   };
 
   const handleCallClick = () => {
-    shootConfetti();
     // Add the desired phone number in the phoneNumber variable
     const phoneNumber = "631-381-5947";
     window.location.href = `tel:${phoneNumber}`;
@@ -44,28 +43,61 @@ export default function Contact() {
     <div className="container mt-5">
       <div className="row">
         <div className="col-md-8 mx-auto contact-container">
-          <h1 className="contact-title">Contact Page</h1>
+          <h1 className="contact-title">Let's Get In Touch!</h1>
           <div className="row">
             <div className="col-md-8 mx-auto">
               <form onSubmit={handleSubmit}>
-                {/* existing form fields */}
-                <div className="text-center">
-                  <button
-                    type="button"
-                    className="btn btn-primary mr-3"
-                    onClick={handleEmailClick}
-                  >
-                    <i className="fas fa-envelope mr-2"></i>Email me
-                  </button>
-                  <button
-                    type="button"
-                    className="btn btn-secondary"
-                    onClick={handleCallClick}
-                  >
-                    <i className="fas fa-phone mr-2"></i>Call me
-                  </button>
+                <div className="form-group">
+                  <label htmlFor="nameInput">Name</label>
+                  <input 
+                    type="text"
+                    className="form-control"
+                    id="nameInput"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                  />
                 </div>
+                <div className="form-group">
+                  <label htmlFor="emailInput">Email address</label>
+                  <input 
+                    type="email" 
+                    className="form-control" 
+                    id="emailInput"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="messageInput">Message</label>
+                  <textarea 
+                    className="form-control" 
+                    id="messageInput" 
+                    rows="3"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    required
+                  ></textarea>
+                </div>
+                <button type="submit" className="btn btn-primary submit-btn">Submit</button>
               </form>
+              <div className="contact-btns mt-4">
+                <button
+                  type="button"
+                  className="btn btn-primary email-btn"
+                  onClick={handleEmailClick}
+                >
+                  <i className="fas fa-envelope mr-2"></i>Email me
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-secondary call-btn"
+                  onClick={handleCallClick}
+                >
+                  <i className="fas fa-phone mr-2"></i>Call me
+                </button>
+              </div>
             </div>
           </div>
         </div>
