389c3a8d75b54b61a6583e2467abf3ef56f6d5fb
Seiya Miyazaki <smsmiyazaki@gmail.com>
2023-05-31 17:28:10 -0400

    Jazzing up the resume skills page

--- src/components/pages/Blog.jsx
+++ src/components/pages/Blog.jsx
@@ -3,42 +3,58 @@ import './Blog.css';
 import BlogPost from './BlogPost';
 import Resume from '../../assets/CV.pdf'
 
-export default function Blog() {
- 
-  const blogPosts = [
-    {
-      title: 'Skills',
-      content: ( <ul>
-        <li>Proficient in JavaScript (ES6+)</li>
-        <li>Experience with React</li>
-        <li>Experience with Node.js and Express</li>
-        <li>Experience with MongoDB and Mongoose</li>
-        <li>Strong understanding of RESTful APIs</li>
-        <li>Knowledge of Git and version control</li>
-        <li>Experience with HTML5 and CSS3</li>
-        <li>Understanding of responsive web design and CSS frameworks like Bootstrap and Tailwind CSS</li>
-        <li>Experience with Jest testing frameworks</li>
-        <li>Experience with build tools like Webpack, Babel, and npm</li>
-        <li>Understanding of web security and best practices</li>
-        <li>Strong problem-solving skills and ability to work independently or in a team</li>
-      </ul>),
-    },
-    {
-      title: 'Click the link below to download my resume',
-      content: <a href={Resume} download>
-      Download Resume
-    </a>,
-    },
-  ];
-
+function InteractiveList({ items }) {
   return (
-    <div className="blog-container  mt-5">
-      <h1 className="blog-title">Curriculum Vitae</h1>
-      <div className="blog-posts">
-        {blogPosts.map((post, index) => (
-          <BlogPost key={index} title={post.title} content={post.content} />
-        ))}
-      </div>
-    </div>
+      <ul className="interactive-list">
+          {items.map((item, index) => 
+              <li key={index} className={`skill-${index}`}>{item}</li>
+          )}
+      </ul>
   );
+}
+
+export default function Blog() {
+    const skills = [
+        "Proficient in JavaScript (ES6+)",
+        "Experience with React",
+        "Experience with Node.js and Express",
+        "Experience with MongoDB and Mongoose",
+        "Strong understanding of RESTful APIs",
+        "Knowledge of Git and version control",
+        "Experience with HTML5 and CSS3",
+        "Understanding of responsive web design and CSS frameworks like Bootstrap and Tailwind CSS",
+        "Experience with Jest testing frameworks",
+        "Experience with build tools like Webpack, Babel, and npm",
+        "Understanding of web security and best practices",
+        "Strong problem-solving skills and ability to work independently or in a team"
+    ];
+
+    const blogPosts = [
+        {
+            title: 'Skills',
+            content: <InteractiveList items={skills} />,
+        },
+        {
+            title: 'Resume',
+            content: (
+                <div className="resume-download-container">
+                    <p>Click the button below to download my resume</p>
+                    <a href={Resume} download className="resume-download-button">
+                        Download Resume
+                    </a>
+                </div>
+            ),
+        },
+    ];
+
+    return (
+        <div className="blog-container  mt-5">
+            <h1 className="blog-title">Curriculum Vitae</h1>
+            <div className="blog-posts">
+                {blogPosts.map((post, index) => (
+                    <BlogPost key={index} title={post.title} content={post.content} />
+                ))}
+            </div>
+        </div>
+    );
 }
